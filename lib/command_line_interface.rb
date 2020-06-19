3523ad230f8e2f74c6f87764ea2db2243beb33c3
smithspencer817 <smithspencer817@gmail.com>
2020-06-19 13:28:46 -0500

    added user authentication, timing, and color functionality

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,3 +1,6 @@
+require "colorize"
+require "tty-prompt"
+
 class CommandLineInterface
 
     def menu
@@ -7,43 +10,69 @@ class CommandLineInterface
     end
     
     def welcome
-        puts "\nWelcome to the Playlist App!"
+        puts "\n---------------------------".green.bold
+        puts "WELCOME TO THE PLAYLIST APP".green.bold
+        puts "---------------------------".green.bold
+        sleep(2)
     end
 
     def login_or_sign_up
-        print "Have you used the app before? y/n "
+        print "\nHave you used the app before? y/n ".green
         response = gets.chomp
         if response == "y"
             get_username_and_password
         elsif response == "n"
             create_new_account
         else
-            puts "\nResponse not recognized. Please try again.\n"
+            puts "\nResponse not recognized. Please try again.\n".red
             login_or_sign_up
         end
     end
 
     def create_new_account
-        print "Please enter a username: "
+        print "Please enter a username: ".green
         username = gets.chomp
-        print "Please enter a password: "
-        password = gets.chomp
+
+        password = TTY::Prompt.new
+        password = password.mask("Please enter your password:".green)
+
         User.create(username: username, password: password)
+
+
     end
 
     def get_username_and_password
-        print "\nPlease enter your username: "
+        print "\nPlease enter your username: ".green
         username = gets.chomp
-        print "Please enter your password: "
-        password = gets.chomp
-        User.find_by(username: username, password: password)
+
+        if User.find_by(username: username) != nil
+            password = TTY::Prompt.new
+            password = password.mask("Please enter your password:".green)
+
+            if User.find_by(username: username, password: password) != nil
+                puts "\n---------------------------".green
+                puts "\nWELCOME BACK, #{username}!".green.bold
+                sleep(2)
+
+            else
+                puts "Password incorrect, please try again".red
+                get_username_and_password
+            end
+
+        else
+            puts "Username not found, please try again".red
+            get_username_and_password
+        end
     end
 
     def display_menu
-        puts "What would you like to do?"
-        puts "\n1. Create new playlist"
-        puts "2. View my playlists"
-        puts "3. Log out"
+        puts "\nWhat would you like to do?".green
+
+        sleep(2)
+        
+        puts "\n1. Create new playlist".green
+        puts "2. View my playlists".green
+        puts "3. Log out".green
         # stretch goal: 4. Add song to another user's queue
         response = gets.chomp
         if response == "1"
@@ -56,19 +85,8 @@ class CommandLineInterface
             # insert method here: log_out
             puts "test"
         else
-            puts "\nResponse not recognized. Please try again.\n"
+            puts "\nResponse not recognized. Please try again.\n".red
             display_menu
         end
     end
-
-
-        
-
-
-
-    
-
-
-    
-
 end
